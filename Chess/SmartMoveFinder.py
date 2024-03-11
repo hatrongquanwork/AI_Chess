9e15f7ad3b4ef5b6935079ea1c16776c7f3c12ab
haquan <trongquan17012002@gmail.com>
2024-03-11 11:52:39 +0700

    first commit

---
+++ Chess/SmartMoveFinder.py
@@ -0,0 +1,165 @@
+import random
+
+pieceScore = {"K": 0, "Q": 10, "R": 5, "B": 3, "N": 3, "p": 1}
+knightScore = [[1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 2, 2, 2, 2, 2, 2, 1],
+                [1, 2, 3, 3, 3, 3, 2, 1],
+                [1, 2, 3, 4, 4, 3, 2, 1],
+                [1, 2, 3, 4, 4, 3, 2, 1],
+                [1, 2, 3, 3, 3, 3, 2, 1],
+                [1, 2, 2, 2, 2, 2, 2, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1]]
+
+bishopScore = [[4, 3, 2, 1, 1, 2, 3, 4],
+                [3, 4, 3, 2, 2, 3, 4, 3],
+                [2, 3, 4, 3, 3, 4, 3, 2],
+                [1, 2, 3, 4, 4, 3, 2, 1],
+                [1, 2, 3, 4, 4, 3, 2, 1],
+                [2, 3, 4, 3, 3, 4, 3, 2],
+                [3, 4, 3, 2, 2, 3, 4, 3],
+                [4, 3, 2, 1, 1, 2, 3, 4]]
+
+queenScore = [[1, 1, 1, 3, 1, 1, 1, 1],
+                [1, 1, 2, 3, 3, 1, 1, 1],
+                [1, 4, 3, 3, 3, 4, 2, 1],
+                [1, 2, 3, 3, 3, 2, 2, 1],
+                [1, 2, 3, 3, 3, 2, 2, 1],
+                [1, 4, 3, 3, 3, 4, 2, 1],
+                [1, 1, 2, 3, 3, 1, 1, 1],
+                [1, 1, 1, 3, 1, 1, 1, 1]]
+
+rookScore = [[4, 3, 4, 4, 4, 4, 3, 4],
+                [4, 4, 4, 4, 4, 4, 4, 4],
+                [1, 1, 2, 3, 3, 2, 1, 1],
+                [1, 2, 3, 4, 4, 3, 2, 1],
+                [1, 2, 3, 4, 4, 3, 2, 1],
+                [1, 1, 2, 2, 2, 2, 1, 1],
+                [4, 4, 4, 4, 4, 4, 4, 4],
+                [4, 3, 4, 4, 4, 4, 3, 4]]
+
+whitePScore = [[8, 8, 8, 8, 8, 8, 8, 8],
+                [8, 8, 8, 8, 8, 8, 8, 8],
+                [5, 6, 6, 7, 7, 6, 6, 5],
+                [2, 3, 3, 5, 5, 3, 3, 2],
+                [1, 2, 3, 4, 4, 3, 2, 1],
+                [1, 1, 2, 3, 3, 2, 1, 1],
+                [1, 1, 1, 0, 0, 1, 1, 1],
+                [0, 0, 0, 0, 0, 0, 0, 0]]
+
+blackPScore = [[0, 0, 0, 0, 0, 0, 0, 0],
+               [1, 1, 1, 0, 0, 1, 1, 1],
+               [1, 1, 2, 3, 3, 2, 1, 1],
+               [1, 2, 3, 4, 4, 3, 2, 1],
+               [2, 3, 3, 5, 5, 3, 3, 2],
+               [5, 6, 6, 7, 7, 6, 6, 5],
+               [8, 8, 8, 8, 8, 8, 8, 8],
+               [8, 8, 8, 8, 8, 8, 8, 8]]
+
+piecePositionScore = {"N": knightScore, 'Q': queenScore, 'B': bishopScore, 'R': rookScore, 'bp': blackPScore, 'wp': whitePScore}
+CHECKMATE = 10000
+STALEMATE = 0
+DEPTH = 4
+
+def findRandomMove(validMoves):
+    return validMoves[random.randint(0, len(validMoves) - 1)]
+
+def findBestMoves(gs, validMoves):
+    turnMultiplier = 1 if gs.whiteToMove else -1
+    opponentMinMaxScore = CHECKMATE
+    bestPlayerMove = None
+    random.shuffle(validMoves)
+    for playerMove in validMoves:
+        gs.makeMove(playerMove)
+        opponentsMoves = gs.getValidMoves()
+        if gs.staleMate:
+            opponentMaxScore = STALEMATE
+        elif gs.checkMate:
+            opponentMaxScore = -CHECKMATE
+        else:
+            opponentMaxScore = -CHECKMATE
+            for opponentsMove in opponentsMoves:
+                gs.makeMove(opponentsMove)
+                gs.getValidMoves()
+                if gs.checkMate:
+                    score = CHECKMATE
+                elif gs.staleMate:
+                    score = STALEMATE
+                else:
+                    score = - turnMultiplier * scoreBoard(gs)
+                if score > opponentMaxScore:
+                    opponentMaxScore = score
+                gs.undoMove()
+        if opponentMaxScore < opponentMinMaxScore:
+            opponentMinMaxScore = opponentMaxScore
+            bestPlayerMove = playerMove
+        gs.undoMove()
+    return bestPlayerMove
+def findBestMove(gs, validMoves):
+    global nextMove
+    nextMove = None
+    random.shuffle(validMoves)
+    #findMoveNegaMax(gs, validMoves, DEPTH, 1 if gs.whiteToMove else -1)
+    findMoveAlphaBetaPuring(gs, validMoves, DEPTH, -CHECKMATE, CHECKMATE, 1 if gs.whiteToMove else -1)
+    return nextMove
+def findMoveNegaMax(gs, validMoves, depth, turnMultiplier):
+    global nextMove
+    if depth == 0:
+        return turnMultiplier * scoreBoard(gs)
+    maxScore = -CHECKMATE
+    for move in validMoves:
+        gs.makeMove(move)
+        nextMoves = gs.getValidMoves()
+        score = -findMoveNegaMax(gs, nextMoves, depth - 1, -turnMultiplier)
+        if score > maxScore:
+            maxScore == score
+            if depth == DEPTH:
+                nextMove = move
+        gs.undoMove()
+    return maxScore
+def findMoveAlphaBetaPuring(gs, validMoves, depth, alpha, beta, turnMultiplier):
+    global nextMove
+    if depth == 0:
+        return turnMultiplier * scoreBoard(gs)
+    maxScore = -CHECKMATE
+    for move in validMoves:
+        gs.makeMove(move)
+        nextMoves = gs.getValidMoves()
+        score = -findMoveAlphaBetaPuring(gs, nextMoves, depth - 1, -alpha, -beta, -turnMultiplier)
+        if score > maxScore:
+            maxScore == score
+            if depth == DEPTH:
+                nextMove = move
+        gs.undoMove()
+        if maxScore > alpha:
+            alpha = maxScore
+        if alpha >= beta:
+            break
+    return maxScore
+
+def scoreBoard(gs):
+    if gs.checkMate:
+        if gs.whiteToMove:
+            return -CHECKMATE
+        else:
+            return CHECKMATE
+    elif gs.staleMate:
+        return STALEMATE
+
+    score = 0
+    for row in range(len(gs.board)):
+        for col in range(len(gs.board[row])):
+            square = gs.board[row][col]
+            if square != '--':
+                piecePS = 0
+                if square[1] != "K":
+                    if square[1] == "p":
+                        piecePS = piecePositionScore[square][row][col]
+                    else:
+                        piecePS = piecePositionScore[square[1]][row][col]
+
+                if square[0] == 'w':
+                    score += pieceScore[square[1]] + piecePS * .1
+                elif square[0] == 'b':
+                    score -= pieceScore[square[1]] + piecePS * .1
+
+    return score
